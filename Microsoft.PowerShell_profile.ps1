d219fc0a5b3cae105c9b4476744e047524abf301
Henry Diaz <89035965+HenryU20201C579@users.noreply.github.com>
2025-08-27 21:00:58 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,3 @@
-Import-Module Terminal-Icons
-Set-PSReadLineOption -PredictionSource History
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/HenryDiaz.omp.json" | Invoke-Expression
+Import-Module -Name Terminal-Icons
 Set-PSReadLineOption -PredictionViewStyle ListView
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\honukai.omp.json" | Invoke-Expression
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
